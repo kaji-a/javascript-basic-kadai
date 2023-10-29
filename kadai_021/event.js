060167e81eaa1361d14605e786014c19a7400293
kaji-a <146502639+kaji-a@users.noreply.github.com>
2023-10-29 22:24:19 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+ // HTML要素を取得
+ const btn = document.getElementById('btn');
+ const text = document.getElementById('text');
+ 
+ // HTML要素がクリックされたときにイベント処理を実行する
+ btn.addEventListener('click', () => {
+    const text2 = document.createElement('h2');
+
+    setTimeout(() => {
+        text.textContent = 'ボタンをクリックしました';
+      }, 2000);
+
+  });
+
